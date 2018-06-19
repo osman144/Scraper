503012210579533c58b00eb326a63a308f938eb6
osman144 <osman144@umn.edu>
2018-06-18 22:24:29 -0500

    setup models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Exporting an object containing the models
+module.exports = {
+    Article: require("./Article"),
+    Note: require("./Note")
+};
+  
--- server.js
+++ server.js
@@ -5,12 +5,29 @@ const express = require ('express');
 const exphbs = require ('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+// Require scraping tool
 const cheerio = require('cheerio');
 const request = require('request');
 
+// Require all models
+const db = require("./models");
+
+
 const PORT = process.env.PORT || 3000;
 
+//Initialize app 
 const app = express();
+
+// Configure middleware
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 app.get('/', function(req,res){
     res.send('Hello World!')
