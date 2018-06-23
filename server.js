7c164a759887d4e6be2e7259b5201da695425126
osman144 <osman144@umn.edu>
2018-06-22 20:13:40 -0500

    set up cheerio

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const exphbs = require ('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // Require scraping tool
+//Cheerio parses markup
 const cheerio = require('cheerio');
+//Axios is a promised-based http library, similar to jQuery's Ajax method
+//Automatically transforms as JSON data
+const axios = require('axios')
 const request = require('request');
-
 // Require all models
 const db = require("./models");
 
@@ -18,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 //Initialize app 
 const app = express();
 
-// Configure middleware
+
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -29,8 +32,13 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 
+//Get route for scraping Reddit NBA website
 app.get('/', function(req,res){
-    res.send('Hello World!')
+    //First grab the body with an HTML request
+    axios.get('https://www.reddit.com/r/nba/').then(function(response){
+        //Load onto Cheerio
+
+    })
 });
 
 app.listen(PORT, function(){
