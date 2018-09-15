84b699c60a1ce7767a6c4c66bdeacb1ffe706d4a
Salah Osman <osman144@umn.edu>
2018-09-15 00:55:05 -0500

    set up handlebars view page

---
+++ routes/view/index.js
@@ -0,0 +1,18 @@
+var router = require("express").Router();
+var db = require("../../models");
+
+// This route renders the homepage
+router.get("/", function(req,res){
+    db.Article
+    .find({saved: false})
+    .sort({date:-1})
+    .then(dbArticles => res.render("index", {articles:dbArticles}))
+});
+
+// This route renders the saved handlebars page 
+router.get("/saved", function(req,res){
+    db.Article
+    .find({saved:true})
+    .sort({date:-1})
+    .then(dbArticles => res.render("saved", {articles:dbArticles}))
+});
