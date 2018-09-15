0cdb89f0c747d2b80574c6c39144e1c4044114f1
Salah Osman <osman144@umn.edu>
2018-09-14 21:17:38 -0500

    remove methods

--- controllers/removeArticle.js
+++ controllers/removeArticle.js
@@ -1,6 +1,17 @@
-var db = require("../models");
+var db = require('../models');
 
 module.exports = {
+    removeAllArticlesDB: function (req, res) {
+        db.Article.remove({}).then(function(dbArticle){
+            //Empty json
+            res.json(dbArticle)
+            console.log('Removed')
+            // res.render('index',{result: dbArticle})
+        }).catch(function(err){
+            // If an error occurred, send it to the client
+            res.json(err)
+        })
+    },
     removeOneDB: function(req, res) {
         db.Article.findOneAndUpdate({ _id: req.params.id }, { saved:false }, { new: true }).then(function(dbArticle) {
             // If able to successfully delete an Article, send it back to the client
@@ -12,4 +23,5 @@ module.exports = {
             res.json(err);
         });
     }
-};
+
+}
--- controllers/removeNote.js
+++ controllers/removeNote.js
@@ -1,6 +1,22 @@
-var db = require("../models");
+var db = require('../models');
 
 module.exports = {
+    removeAllNotesDB: function(req,res){
+        db.Note.remove({}, function(error, response) {
+            // Log any errors to the console
+            if (error) {
+              console.log(error);
+              res.send(error);
+            }
+            else {
+              // Otherwise, send the mongojs response to the browser
+              // This will fire off the success function of the ajax request
+              console.log(response);
+              res.send(response);
+            }
+        });
+    },
+
     removeOneNoteDB: function(req, res) {
         db.Note.remove(
             {
@@ -20,4 +36,5 @@ module.exports = {
             }
         });
     }
-};
+
+}
