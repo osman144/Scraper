d115ecc27516a967fb6167e8031f832923f1efd9
Salah Osman <osman144@umn.edu>
2018-09-15 02:13:23 -0500

    set up server side

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const express = require ('express');
 const exphbs = require ('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const request = require('request');
 // Require all models
 const db = require("./models");
 
@@ -15,17 +14,24 @@ const PORT = process.env.PORT || 3000;
 //Initialize app 
 const app = express();
 
+// Require routes
+const routes = require("./routes");
 
-//Handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
 
+// Connect Handlebars to Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Use bodyParser in our app
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Have every request go through route middleware
+app.use(routes);
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -35,61 +41,6 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlin
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-//GET 
-// Route for getting all Articles from the db
-app.get('/home', function(req,res){
-    // Grab every document in the Articles collection
-    db.Article.find({}).then(function(dbArticle){
-        // res.json(dbArticle)
-        console.log('Retrieved all articles')
-        console.log(dbArticle)
-        res.render('index',{dbArticle})
-    }).catch(function(err){
-        // If an error occurred, send it to the client
-        res.json(err)
-    })
-    
-})
-
-// GET
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in db...
-    db.Article.findOne({ _id: req.params.id})
-      // ..and populate all of the notes associated with it
-      .populate("note")
-      .then(function(dbArticle) {
-        // If able to successfully find an Article with the given id, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-});
-  
-
-// Add notes 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body).then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-        }).then(function(dbArticle) {
-            // If able to successfully update an Article, send it back to the client
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
-});
-
-
-app.get
 app.listen(process.env.PORT || 3000, function(){
     console.log("App running on port " + PORT + "!");
 });
